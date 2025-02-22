b8914e93582aecf589ac33d5ce29887e4b6ee58d
Dylan Boyer <85534612+dylanboyer@users.noreply.github.com>
2025-02-21 22:20:06 -0500

    starter site

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routes from './routes';
 
-import { createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from 'react-router-dom';
 const router = createBrowserRouter(createRoutesFromElements(Routes()));
 
 // Render the app
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,17 @@
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom';
 
 // Import layout
-import MainLayout from "./layouts/main-layout";
+import MainLayout from './layouts/main-layout';
 
 // Import all pages
-import Home from "./pages/Home";
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 export default function Routes() {
-	return (
-		<Route element={<MainLayout />}>
-			<Route path="/" element={<Home />} />
-		</Route>
-	);
+  return (
+    <Route element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+    </Route>
+  );
 }
