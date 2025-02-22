d709f7ebf7bc6dc8b3502ee2c8436e7609a05553
Dylan Boyer <85534612+dylanboyer@users.noreply.github.com>
2025-02-22 01:08:03 -0500

    Working with shadcn

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,23 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function Login() {
 
     return (
-      <div>This is login!</div>
+      <div>
+        <h1>This is login!</h1>
+        <Label>Login</Label>
+        <Input
+          className="bg-light-gray placeholder:text-dark-gray dark:bg-darker-gray dark:placeholder:text-light-gray"
+					placeholder="email"/>
+        <Label>Password</Label>
+          <Input
+          className="bg-light-gray placeholder:text-dark-gray dark:bg-darker-gray dark:placeholder:text-light-gray"
+					placeholder="password"
+          type="password"/>
+          <Button>Login</Button>
+        </div>
     )
   }
   
