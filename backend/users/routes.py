6992232c90dfaf8e7c37c239ee2823edda84e440
hasting7 <hasting7@purdue.edu>
2025-02-22 18:00:30 -0500

    session

--- backend/users/routes.py
+++ backend/users/routes.py
@@ -1,5 +1,6 @@
 from backend.users import bp
 from flask import jsonify, request, session
+import threading
 
 from Database import Users
 from Database import Exercises
@@ -14,7 +15,7 @@ def get_all_ids():
 @bp.route('/session')
 def give_session_data():
 	if session.get("user_id"):
-		return jsonify({'is_valid' : True}), 200
+		return jsonify({'is_valid' : True , 'user_id' : session.get("user_id")}), 200
 	return jsonify({'is_valid' : False}), 400
 
 # GET /users/all
@@ -68,17 +69,22 @@ def user_log_out():
 @bp.route('/create', methods=['POST'])
 def create_user():
 	data = request.json
+
+	# add all the defualt exersise in the background
+	def background_task(user_id):
+		# This function will run in the background.
+		try:
+			for lift in Exercises.Default_Exercises:
+				status = Exercises.CreateAllExerciseRanges(user_id, [0], lift)
+				print('status on create', status, 'adding', lift)
+		except Exception as e:
+			print(f"Error while adding exercises: {e}")
+
 	try:
 		new_id = Users.CreateUser(data)
 		session['user_id'] = new_id
 
-		# add all the basic lifts
-
-		for lift in Exercises.Default_Exercises:
-			status = Exercises.CreateAllExerciseRanges(new_id, [0], lift)
-			print('status on create', status, 'adding',lift)
-
-
+		threading.Thread(target=background_task, args=(new_id,)).start()
 
 		return jsonify({'status' : 'ok', 'u_id' : new_id}), 200
 	except Exception as e:
