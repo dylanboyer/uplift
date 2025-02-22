6aaa67b842416d219ae6f4922a2f979fbd9e275a
hasting7 <hasting7@purdue.edu>
2025-02-22 18:38:23 -0500

    fixing profile

--- src/pages/private-profile.tsx
+++ src/pages/private-profile.tsx
@@ -1,22 +1,25 @@
 import { useParams } from "react-router-dom";
 import { useGetUserID } from "@/hooks/use-user"; // Assuming the hook is in a separate file
+import { useGetUsedExercises } from "@/hooks/use-exercise";
 import Widget from "@/components/widget";
 
 import "@/components/widget.css";
 
-export default function ProfileRouter() {
-  const { userID, loading, error } = useGetUserID(); // Use the custom hook
+export default function PrivateProfile() {
+  const { userID, loading_user, error_user } = useGetUserID(); // Use the custom hook
+  const { exercises, loading_ex, error_ex } = useGetUsedExercises();
 
   // If session is still being checked, show loading state
-  if (loading) {
+  if (loading_user || loading_ex) {
     return <p>Loading session data...</p>;
   }
 
   // Handle errors if fetching session data fails
-  if (error) {
+  if (error_user || error_ex) {
     return <p>Error fetching session data</p>;
   }
   console.log(userID)
+  console.log(exercises)
 
   return (
     <div>
