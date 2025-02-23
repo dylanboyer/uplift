d618053ad582080f6c8f85d01969e04dceda6888
hasting7 <hasting7@purdue.edu>
2025-02-23 02:58:05 -0500

    add buttons

--- src/components/ui/exercise-button.tsx
+++ src/components/ui/exercise-button.tsx
@@ -23,60 +23,63 @@ const ExerciseButton: React.FC<ExerciseButtonProps> = ({
       {/* Main Exercise Button */}
       <button
         onClick={onClick}
-        className={`w-52 h-52 p-4 rounded-lg transition-colors ${
+        className={`w-52 h-52 p-4 rounded-lg transition-colors z-10 ${
           isNew
-            ? "bg-green-600 rounded-full text-white text-4xl font-bold hover:bg-green-800"
-            : "bg-zinc-500 text-white hover:bg-zinc-600 font-semibold text-xl"
-        } flex items-center justify-center`}
+            ? "bg-green-700 rounded-full text-white text-4xl font-bold hover:bg-green-600"
+            : "bg-zinc-700 text-white hover:bg-zinc-600 font-semibold text-xl"
+        } flex items-center justify-center relative`}
       >
         <span className="text-center">{name}</span>
+
+        {/* Action Buttons Container - Positioned at the bottom of the exercise button */}
+        {!isNew && (
+          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
+            {/* Add New Button (Green Plus) with Tooltip */}
+            <div className="relative group">
+              <button
+                onClick={onAddNew}
+                className="p-2 rounded-md text-white transition-colors hover:bg-green-400"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+              </button>
+              <div className="absolute bottom-8 left-0 hidden group-hover:block bg-black text-white text-xs rounded-md p-1 whitespace-nowrap z-50">
+                Add New Exercise
+              </div>
+            </div>
+            {/* Modify Button (Blue) with Tooltip */}
+            <div className="relative group">
+              <button
+                onClick={onModify}
+                className="p-2 rounded-md text-white transition-colors hover:bg-blue-400"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                </svg>
+              </button>
+              <div className="absolute bottom-8 left-0 hidden group-hover:block bg-black text-white text-xs rounded-md p-1 whitespace-nowrap z-50">
+                Modify Exercise
+              </div>
+            </div>
+            {/* Delete Button with Tooltip */}
+            <div className="relative group">
+              <button
+                onClick={onDelete}
+                className="p-2 rounded-md text-white transition-colors hover:bg-red-400"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+              </button>
+              <div className="absolute bottom-8 right-0 hidden group-hover:block bg-black text-white text-xs rounded-md p-1 whitespace-nowrap z-50">
+                Delete Exercise
+              </div>
+            </div>
+
+          </div>
+        )}
       </button>
-
-      {/* Actions Buttons Container - Bottom right of the exercise button */}
-      {!isNew && (
-        <div className="absolute bottom-0 right-0 flex space-x-2 p-2">
-          {/* Delete Button with Tooltip */}
-          <div className="relative group">
-            <button
-              onClick={onDelete}
-              className="p-2 bg-red-500 text-white rounded-full hover:bg-red-600 transition-colors"
-            >
-              🗑️
-            </button>
-            <div className="absolute top-8 left-0 hidden group-hover:block bg-black text-white text-xs rounded-md p-1">
-              Delete Exercise
-            </div>
-          </div>
-
-          {/* Modify Button (Blue) with Tooltip */}
-          <div className="relative group">
-            <button
-              onClick={onModify}
-              className="p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors"
-            >
-              ✏️
-            </button>
-            <div className="absolute top-8 right-0 hidden group-hover:block bg-black text-white text-xs rounded-md p-1">
-              Modify Exercise
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Add New Button (Green Plus) with Tooltip */}
-      {!isNew && (
-        <div className="relative group">
-          <button
-            onClick={onAddNew}
-            className="p-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition-colors"
-          >
-            ➕
-          </button>
-          <div className="absolute top-8 right-0 hidden group-hover:block bg-black text-white text-xs rounded-md p-1">
-            Add New Exercise
-          </div>
-        </div>
-      )}
     </div>
   );
 };
